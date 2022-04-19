b82b6beb0ffb6c10eeb8de6b9222a231ff0db7af
DarthJake <j.e.ellsworth.2000@gmail.com>
2022-04-19 11:11:44 -0700

    Made it so it doesnt download the installer to calculate the checksum. Just fetch them.

--- wpilib/update.ps1
+++ wpilib/update.ps1
@@ -20,10 +20,7 @@ function global:au_SearchReplace {
 }
 
 # Not called if there is no update 
-function global:au_BeforeUpdate { 
-    $Latest.Checksum32 = Get-RemoteChecksum $Latest.URL32
-    $Latest.Checksum64 = Get-RemoteChecksum $Latest.URL64
-}
+function global:au_BeforeUpdate {}
 
 function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases
@@ -31,8 +28,8 @@ function global:au_GetLatest {
     # Get URLs of latest installers
     $regex32 = "WPILib_Windows32-.*\.iso"
     $regex64 = "WPILib_Windows64-.*\.iso"
-    $url32 = $download_page.links | ? href -match $regex32 | select -First 1 -expand href
-    $url64 = $download_page.links | ? href -match $regex64 | select -First 1 -expand href
+    $url32 = $download_page.links | Where-Object href -match $regex32 | Select-Object -First 1 -expand href
+    $url64 = $download_page.links | Where-Object href -match $regex64 | Select-Object -First 1 -expand href
     $url32 = $domain + $url32
     $url64 = $domain + $url64
 
@@ -41,6 +38,17 @@ function global:au_GetLatest {
     $versionRegex = "(?:WPILib_Windows64-)(?<Version>.*)(?:\.iso)"
     $url64 -match $versionRegex | Out-Null
     $version = $Matches.Version
+
+    # Get checksums of latest installers
+    # 32
+    $download_page.RawContent | ForEach-Object {$_ -replace "<[^ ]+>", "`n"} | `
+        ForEach-Object {$_ -match "(?<checksum32>[0-9a-fA-F]{64})(?:\s*Win32\/WPILib_Windows32-$version.iso)"}
+    $Latest.Checksum32 = $Matches.checksum32
+
+    # 64
+    $download_page.RawContent | ForEach-Object {$_ -replace "<[^ ]+>", "`n"} | `
+        ForEach-Object {$_ -match "(?<checksum64>[0-9a-fA-F]{64})(?:\s*Win64\/WPILib_Windows64-$version.iso)"}
+    $Latest.Checksum64 = $Matches.checksum64
 
     # Make release notes URL
     $releaseNotesUrl = "$releases/tag/v${version}"
